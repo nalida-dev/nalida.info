bea651e91975897aed0d1e3ccdb4ad1394c62320
inmoonlight <mjihyung@gmail.com>
2019-01-26 16:34:21 +0900

    Add etc workshop

--- nalida-info/src/app/workshop-posts.ts
+++ nalida-info/src/app/workshop-posts.ts
@@ -1,27 +1,32 @@
 export const POSTS = [
   {
-    title: '날리다: Classic',
+    title: '날리다 : CLASSIC',
     image: 'images/workshop_classic.png',
     description: 'sample description'
   },
   {
-    title: '날리다: Color',
+    title: '날리다 : COLOR',
     image: 'images/workshop_color.jpg',
     description: 'sample description'
   },
   {
-    title: '같이 혼자 여행',
+    title: '날리다: 같이혼자여행',
     image: 'images/workshop_gathonyeo.jpg',
     description: 'sample description'
   },
   {
-    title: '날리냥: Portable',
+    title: '날리다 : PORTABLE',
     image: 'images/workshop_nalinyang.png',
     description: 'sample description'
   },
   {
-    title: '날리다: World',
+    title: '날리다 : WORLD',
     image: 'images/workshop_world.jpg',
     description: 'sample description'
+  },
+  {
+    title: '날리다 : 기타',
+    image: 'images/workshop_etc.jpg',
+    description: '비일상, 허니번 등'
   }
 ]
