987576219e8be19c58115553f34c0872b6072ca0
javelinsman <javelinsman@snu.ac.kr>
2019-02-03 16:23:59 +0900

    convert description to plain text and show it truncated. Link to proper url

--- nalida-info/src/app/nalida-story/nalida-story-post-preview/nalida-story-post-preview.component.ts
+++ nalida-info/src/app/nalida-story/nalida-story-post-preview/nalida-story-post-preview.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from 'src/app/post';
+import * as showdown from 'showdown';
 
 @Component({
   selector: 'app-nalida-story-post-preview',
@@ -9,10 +10,16 @@ import { Post } from 'src/app/post';
 export class NalidaStoryPostPreviewComponent implements OnInit {
 
   @Input() post: Post;
+  markdownConverter: showdown.Converter;
+  convertedDescription: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.markdownConverter = new showdown.Converter();
+    const temp = document.createElement('div');
+    temp.innerHTML = this.markdownConverter.makeHtml(this.post.description);
+    this.convertedDescription = temp.innerText;
   }
 
 }
