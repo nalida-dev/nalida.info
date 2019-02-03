dee90b308bcb094ca4ce6fb70566f555e37c6edd
javelinsman <javelinsman@snu.ac.kr>
2019-02-03 15:52:23 +0900

    add attribute *id* to Post

--- nalida-info/src/app/mock-posts.ts
+++ nalida-info/src/app/mock-posts.ts
@@ -2,31 +2,35 @@ import { Post } from './post';
 
 export const POSTS: Post[] = [
   {
+    id: 0,
     title: '자기이해 길라잡이',
     image: 'images/story_1_image.png',
     description: '니나니나',
     tags: ['날리다', '자기이해'],
-    author: "kiroong"
+    author: 'kiroong'
   },
   {
+    id: 1,
     title: '브런치의 대가',
     image: 'images/story_1_image.png',
     description: '글을 쓴다는 것은 나의 인생',
     tags: ['자기이해', '문지형', '박형서', '이주헌'],
-    author: "kiroong"
+    author: 'kiroong'
   },
   {
+    id: 2,
     title: '자기이해의 왕',
     image: 'images/story_1_image.png',
     description: '답은 언제나 내 안에 있다',
     tags: ['문지형'],
-    author: "kiroong"
+    author: 'kiroong'
   },
   {
+    id: 3,
     title: '방탈출 카페 리스트',
     image: 'images/story_1_image.png',
     description: '방탈출 허쉴~~',
     tags: ['방탈출', '서울_이스케이프', '히히하'],
-    author: "kiroong"
+    author: 'kiroong'
   }
 ];
